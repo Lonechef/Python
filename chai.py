81567cb7bd48b8681f284e608d81f0faa3d26e2b
Saumya Parikh <saumya.parikh@arthmate.co.in>
2025-02-20 22:14:04 +0530

    Python Course

---
+++ chai.py
@@ -0,0 +1,6 @@
+# Now basically I want to call my  chai method which is in hello_chai I want to call that 
+# file name and the fucntion you want to import
+# And simply invoke that method 
+# SO when we do this by side anothe folder called as pycache is created
+from hello_chai import chai
+chai("TMKOC")
---
+++ hello_chai.py
@@ -0,0 +1,9 @@
+print("chai aur code")
+
+# def : is basically defiantions 
+# Here Simply we are defining a fucntion which takes n as the input and prints n
+def chai(n):
+    print(n)
+
+# THen calling my function
+chai("Masala Tea")
