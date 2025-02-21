b146076cad5d841ca6e560f24019a5e6c9552430
Saumya Parikh <saumya.parikh@arthmate.co.in>
2025-02-21 11:32:33 +0530

    Data TYpes

---
+++ 02/datatypes.py
@@ -0,0 +1,39 @@
+# Object types or Data Types
+# Numbers : 
+# String : EIther singl or double quote all will work in Python
+# List in Python
+myList = [1,2]
+print(myList)
+advList = [1,2,['three'],4.5]
+print(advList)
+# In python similar to list we have the tuple  they are defined by parentheisis ()
+
+myTuple = (1,2,'span'),tuple('spam')
+print(myTuple) #So output will be (1,2,'span'),('s','p','a','m'))
+
+# Dictionaties : They are mnetioned in the curly Braces
+
+Dictionary = {'food':'spam',"taste":"yum"}
+print(Dictionary)
+
+# Set : No  duplicates 
+
+Set =  set('abc'),{'a','b','c'}
+print(Set) #So here the Out put will be  ({'a','b','c'},{'a','c','b'})
+
+# Some calculations using Python is very easy
+print(2**100) # This basically means as 2^100
+# So doing below will allow us to make any random value
+import random
+print(random.random())
+# YOu can use a method called as random choice
+print(random.choice([1,2,3,4,5])) 
+
+username = "Saumya"
+print(len(username))
+# In python String is treated similarly as list
+username = "chaiaurcode"
+# SO this is basically treating 2 as out index
+print(username[2]) 
+# Now basically in this 
+
