bc887b4a09412348e8fb8aabc7311bc609f853d1
Saumya Parikh <saumya.parikh@arthmate.co.in>
2025-02-23 20:15:04 +0530

    Numbers in depth

---
+++ 04/Numbersindepth.py
@@ -0,0 +1,14 @@
+1 == 2 < 3 # This is basically 1==2 and 2<3 SO this comparisoin done and ans will be False
+print(1 == 2 <3) #False
+# SImilar to floor and ceiling we have a trunc
+import math
+print(math.trunc(2.8))  #Basically in this if we do trunc then in that case it will move towards 0 
+print(0o20) #This is basically called as octal literate  Output will be 16 this is basically base to the 8 (Here numbers are between 0 to 7)
+# So basically for octal it is o and similat to octeal  we have hexal basically hexal means h x
+print(0x10) #This is basically hexal  ans similary we have binary basically it will be  b
+# We can convert any number to octal
+print(oct(64)) #This will give me the octal number for 64
+print(hex(69)) #This gives me hexal value
+import random
+print(random.randint(1,100)) #Basically this will give me the random integer between 1 to 100 only
+# THere is another method called as random.shuffle() basically this helps to randomly shuffle elements in the array
